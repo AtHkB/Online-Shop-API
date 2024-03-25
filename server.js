8b751cde903fc29461a9d71518c5a34f193f3f8a
Ata Hakkak <ata.hakkak@gmail.com>
2024-03-25 11:11:29 +0100

    basic setups

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./dbinit");
+const dotenv = require("dotenv");
+//const jsonData = require("./file.json");
+const apiFightResult = require("./routes/fight-result.router");
+const apiPokemon = require("./routes/pokemon.router");
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 3005;
+
+//use CORS
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+//use router
+app.use([apiFightResult, apiPokemon]);
+
+// init db
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
